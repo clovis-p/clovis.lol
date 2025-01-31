b15211e51529ccded1b89ddbb254dba7c237b000
clovis <clovis-p@proton.me>
2025-01-31 13:45:32 -0500

    Add animated navbar

---
+++ js/main.js
@@ -0,0 +1,99 @@
+"use strict";
+
+function init() {
+    $("h1").css("text-align", "center")
+        .css("font-size", "30px");
+
+    $(".main-li").css("display", "inline-flex")
+        .css("margin-top", "10px")
+        .css("margin-bottom", "10px")
+        .css("margin-left", "auto")
+        .css("margin-right", "auto")
+        .children("a").css("padding", "10px")
+        .css("font-size", "24px")
+        .on("click", turnListIntoNavbar);
+
+    $("#main-nav ul").css("display", "flex")
+        .css("flex-direction", "column")
+        .css("list-style-type", "none")
+        .css("margin", "auto");
+
+    $("nav").css("display", "block");
+
+    $("#js-pong-link").on("click", function () {
+        const fullscreenMask = $("#fullscreen-mask");
+        $("<iframe>").prop("src", "./projets/js-pong/pong.html")
+            .prop("id", "game-area")
+            .appendTo(fullscreenMask);
+    });
+
+    $("#blocks-link").on("click", function () {
+        const fullscreenMask = $("#fullscreen-mask");
+        $("<iframe>").prop("src", "./projets/blocks-emscripten/index.html")
+            .prop("id", "game-area")
+            .appendTo(fullscreenMask);
+    });
+}
+
+function turnListIntoNavbar() {
+    const fullscreenMask = $("<div>");
+    fullscreenMask.css("position", "absolute")
+        .css("top", "0")
+        .css("bottom", "0")
+        .css("left", "0")
+        .css("right", "0")
+        .prop("id", "fullscreen-mask")
+        //.css("opacity", "0")
+        .appendTo("body");
+
+    $("h1").css("opacity", "0")
+        .css("transition", "opacity 0.2s");
+
+    const mainNavUl = $("#main-nav ul");
+    const mainNavUlClone = mainNavUl.clone();
+    mainNavUlClone.css("display", "flex")
+        .css("flex-direction", "row")
+        .css("flex-wrap", "wrap")
+        .css("list-style-type", "none")
+        .css("margin", "auto")
+        .prop("id", "ul-clone");
+
+    const mainLiClone = mainNavUlClone.children("li");
+    mainLiClone.css("display", "inline-flex")
+        .css("margin-top", "10px")
+        .css("margin-bottom", "10px")
+        .css("margin-left", "10px")
+        .css("margin-right", "10px");
+
+    mainNavUlClone.appendTo(fullscreenMask)
+        .css("opacity", "0");
+
+    $("#main-ul .main-li").each(function(_, element) {
+        const id = $(element).attr("id");
+        console.log(id);
+
+        const liPos = $(element).offset();
+        const liClonePos = $("#ul-clone #" + id).offset();
+
+        const deltaX = liClonePos.left - liPos.left;
+        const deltaY = liClonePos.top - liPos.top;
+
+        console.log(id + ": deltaX: " + deltaX + " deltaY: " + deltaY);
+
+        $(element).css("transform","translate( " + deltaX + "px, " + deltaY + "px)")
+            .css("transition", "transform 0.25s")
+            .css("transition-delay", (0.1 * parseInt(id.charAt(2)) - 0.1) + "s");
+    });
+
+    mainNavUlClone.css("display", "none")
+        .css("opacity", "1");
+
+    setTimeout(function () {
+        $("#main-ul").remove();
+        mainNavUlClone.css("display", "flex");
+    }, 900);
+}
+
+$(document).ready(function () {
+    init();
+});
